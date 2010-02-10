b9bba220fe73817ef749dd125ba7386223be78f3
unknown <hendra@.(none)>
2010-02-10 19:05:17 +0700

    Initial widget support Initial shortcode support

--- warung.php
+++ warung.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: warung
+Plugin Name: Warung
 Plugin URI: http://warung.org/
 Description: Simple shop plugins.
 Author: Hendra Setiawan
@@ -8,6 +8,7 @@ Version: 1.0
 Author URI: http://hendra.org/
 */
 
+// version check
 global $wp_version;
 
 $exit_msg = 'Warung, require WordPress 2.6 or newer.
@@ -18,14 +19,42 @@ if (version_compare($wp_version, "2.6", "<")) {
     exit($exit_msg);
 }
 
+// init
 require_once 'warung_class.php';
 
-// create instance of our class
+function warungCart($args=array()) {
+    extract($args);
+
+    echo $before_widget;
+    echo $before_title .'Keranjang Belanja'. $after_title;
+    if (isset($_POST['product'])) {
+        if (isset($_SESSION["warungCart"])) {
+            $_SESSION["warungCart"] += 1;
+        } else {
+            $_SESSION["warungCart"] = 1;
+        }
+    }
+
+    if (!isset($_SESSION["warungCart"])) {
+        $_SESSION["warungCart"] = 0;
+    }
+    ?>
+    <p>Jumlah item: <?php echo $_SESSION["warungCart"]?></p>
+    <?
+    echo $after_widget;
+}
+
+function my_init() {
+    session_start();
+    register_sidebar_widget('Warung Cart', 'warungCart');
+}
+
 if (class_exists("Warung")) {
     $warung = new Warung();
     if (isset($warung)) {
         register_activation_hook(__FILE__, array(&$warung,'install'));
     }
+    add_action('init', 'my_init');
 } else {
     exit ("Class Warung does not exist!");
 }
--- warung_class.php
+++ warung_class.php
@@ -15,14 +15,110 @@ class Warung {
     function Warung() {
         $this->pluginUrl = trailingslashit(WP_PLUGIN_URL+'/'+dirname(plugin_basename(__FILE__)));
         add_shortcode('warung', array(&$this, 'shortCode'));
+        add_action('init', array(&$this, 'init'));
+    }
+
+    function init() {
+        register_sidebar_widget('Warung Cart', array(&$this, 'warungCart'));
     }
 
     function install() {
+        
+    }
 
+    // [warung name="Avocado" price="10000|20000" type="120x200|160x200"]
+    function shortCode($params, $content=null) {
+
+        extract(shortcode_atts(array(
+            'name' => null,
+            'price' => null,
+            'type' => null
+            ), $params));
+       
+        $ret = '';
+
+        $vals = $this->splitMultipleType($name, $price, $type);
+
+        if (count($vals) > 1) {
+            // show select
+            // - name1|type 1|price1, name1 - type1 - price
+            // - name2|type 2|price2, name2 - type2 = price
+            // - name3|type 3|price3, name3 - type3 - price
+            // +
+            // add to cart
+            $ret =
+                '<div id="wr_product">'.
+                    '<form action="#" method="POST">'.
+                        '<select name="product">';
+            foreach ($vals as $n => $v) {
+                $ret .= '<option value="'.$v.'">'.$n.'</option>';
+            }
+            $ret .= '</select>'.
+                        '<input type="submit" value="Add to cart"/>'.
+                    '</form>'.
+                '</div>';
+        } else {
+            // just show name - price + add to cart
+            $ret =
+                '<div id="w_product">'.
+                    '<form action="#" method="POST">'.
+                        '<input type="hidden" name="product" value="'.$vals[key($vals)].'"/>'.
+                        '<input type="submit" value="Add to cart"/>'.
+                    '</form>'.
+                '</div>';
+        }
+
+
+
+        return $ret;
     }
-    
-    function shortCode() {
-        return "add-to-cart";
+
+    function splitMultipleType($name, $price, $type) {
+        $ret = array();
+
+        $types = null;
+        $prices = null;
+
+        // get type
+        if (! is_null($type) ) {
+            $types = preg_split('/\|/', $type);
+        }
+        // get price
+        if (! is_null($price) ) {
+            $prices= preg_split('/\|/', $price);
+        }
+
+        if (count($types) > 1) {
+            // key(name) => value
+            // name - type @ price => name|price|type
+            if (count($types) == count($price)) {
+                $i=0;
+                foreach ($types as $t) {
+                    $ret[$name.'-'.$t.'@'.$prices[$i]] = $name.'|'.$prices[$i].'|'.$t;
+                    $i++;
+                }
+            } else {
+                $i=0;
+                foreach ($types as $t) {
+                    $p = $prices[min($i, count($prices)-1)];
+                    $ret[$name.'-'.$t.'@'.$p] = $name.'|'.$p.'|'.$t;
+                    $i++;
+                }
+            }
+        } else {
+            $ret[$name.'@'.$price] = $name.'|'.$price;
+        }
+
+        return $ret;
     }
+
+    function warungCart($args=array()) {
+        extract($args);
+        echo $before_widget;
+        echo $before_title .'OCRE'. $after_title;
+        echo "<p>OYEEEE</p>";
+        echo $after_widget;
+    }
+
 }
 ?>
