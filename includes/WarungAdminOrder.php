6bcb37c9b6960832d232654622d31b99d49041dd
sihendra <sihendra@gmail.com>
2011-04-15 07:08:57 +0700

    WarungAdminOrder: - Fix table Styling

--- includes/WarungAdminOrder.php
+++ includes/WarungAdminOrder.php
@@ -16,11 +16,11 @@ class WarungAdminOrder {
         ?>
 <div class="wrap">
     <h2>Order</h2>
-    <table>
+    <table class="wp-list-table widefat">
         <thead>
             <tr>
                 <th scope="col" id="cb" class="manage-column column-cb check-column" style=""><input type="checkbox"></th>
-                <th scope="col" id="id" class="manage-column column-title sortable desc" style=""><a href="http://localhost/%7Ehendra/wp/wp-admin/edit.php?orderby=title&amp;order=asc"><span>ID</span><span class="sorting-indicator"></span></a></th>
+                <th scope="col" id="id" class="manage-column num sortable desc" style=""><a href="http://localhost/%7Ehendra/wp/wp-admin/edit.php?orderby=title&amp;order=asc"><span>ID</span><span class="sorting-indicator"></span></a></th>
                 <th scope="col" id="buyer" class="manage-column column-author sortable desc" style=""><a href="http://localhost/%7Ehendra/wp/wp-admin/edit.php?orderby=author&amp;order=asc"><span>Buyer</span><span class="sorting-indicator"></span></a></th>
                 <th scope="col" id="items" class="manage-column column-categories" style="">Items</th>
                 <th scope="col" id="shipping" class="manage-column column-tags" style="">Shipping</th>
@@ -32,7 +32,7 @@ class WarungAdminOrder {
 	<tfoot>
             <tr>
                 <th scope="col" id="cb" class="manage-column column-cb check-column" style=""><input type="checkbox"></th>
-                <th scope="col" id="id" class="manage-column column-title sortable desc" style=""><a href="http://localhost/%7Ehendra/wp/wp-admin/edit.php?orderby=title&amp;order=asc"><span>ID</span><span class="sorting-indicator"></span></a></th>
+                <th scope="col" id="id" class="manage-column num sortable desc" style=""><a href="http://localhost/%7Ehendra/wp/wp-admin/edit.php?orderby=title&amp;order=asc"><span>ID</span><span class="sorting-indicator"></span></a></th>
                 <th scope="col" id="buyer" class="manage-column column-author sortable desc" style=""><a href="http://localhost/%7Ehendra/wp/wp-admin/edit.php?orderby=author&amp;order=asc"><span>Buyer</span><span class="sorting-indicator"></span></a></th>
                 <th scope="col" id="items" class="manage-column column-categories" style="">Items</th>
                 <th scope="col" id="shipping" class="manage-column column-tags" style="">Shipping</th>
@@ -47,7 +47,7 @@ class WarungAdminOrder {
 ?>
         <tr id="order-<?=$order->id?>" class="alternate author-self status-publish format-default iedit" valign="top">
             <th scope="row" class="check-column"><input name="post[]" value="1" type="checkbox"></th>
-            <td class="post-title page-title column-title"><strong><a class="row-title" href="http://localhost/%7Ehendra/wp/wp-admin/post.php?post=1&amp;action=edit" title="Edit “Hello world!”"><?=$order->id?></a></strong></td>
+            <td class=""><strong><a class="" href="http://localhost/%7Ehendra/wp/wp-admin/post.php?post=1&amp;action=edit" title="Edit “Hello world!”"><?=$order->id?></a></strong></td>
             <td class="author column-author"><a href="edit.php?post_type=post&amp;author=1"><?=$order->getBuyerName()?></a></td>
             <td class="categories column-categories"><a href="edit.php?post_type=post&amp;category_name=uncategorized"><?=$order->getItemsSummary()?></a></td>
             <td class="tags column-tags"><?=$order->getShippingAddress()?></td>
